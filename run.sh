6a480de82c4c5e5d4a9450e321463cb1e60e4880
darkextratoasty <joshuaeblackburn@gmail.com>
2023-04-24 21:50:22 +0000

    remove "&" in run.sh

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source $SCRIPT_DIR/.credentials
 #launch heartbeat python script
-/usr/bin/python3 $SCRIPT_DIR/heartbeat.py $USERNAME $PASSWORD &
+/usr/bin/python3 $SCRIPT_DIR/heartbeat.py $USERNAME $PASSWORD
